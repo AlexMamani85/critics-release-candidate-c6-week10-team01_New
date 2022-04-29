980ffeed3819b902cefe8c113539201517706798
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-04-29 17:17:56 -0500

    feat:seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_29_180329) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_220014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_29_180329) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "criticable_type", null: false
+    t.bigint "criticable_id", null: false
+    t.index ["criticable_type", "criticable_id"], name: "index_critics_on_criticable"
     t.index ["user_id"], name: "index_critics_on_user_id"
   end
 
